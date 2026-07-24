cb133aef0afd1e4c495283eb2bf7347cb2d34eff
OASI-dev <gamingabcde25@gmail.com>
2026-07-24 20:16:50 +0600

    File organizer-Project 1

---
+++ file_organizer.py
@@ -0,0 +1,43 @@
+import os
+import shutil
+
+folder_path = "test_folder"
+
+category_map = {
+    ".pdf" : "PDFs",
+    ".jpg" : "Images",
+    ".jpeg" :"Images",
+    ".png" : "Images",
+    ".gif" : "Images",
+    ".txt" : "Documents",
+    ".docs" : "Documents",
+    ".doc" : "Documents",
+    "mp3" : "Audio",
+    ".wav" : "Audio",
+    ".csv" : "Spreadsheets",
+    ".xlsx" : "Spreadsheets",
+    ".exe" : "Executables"
+}
+
+def main():
+    for filename in os.listdir(folder_path):
+        source_path = os.path.join(folder_path,filename)
+
+        if not os.path.isfile(source_path):
+            continue
+
+        _,extension = os.path.splitext(filename)
+        extension = extension.lower()
+
+        category = category_map.get(extension,"other")
+
+        destination_folder = os.path.join(folder_path,category)
+        os.makedirs(destination_folder,exist_ok = True)
+
+        destination_path = os.path.join(destination_folder, filename)
+        shutil.move(source_path,destination_path)
+
+        print(f"Moved {filename} -> {category}/")
+
+if __name__ == "__main__":
+    main()
